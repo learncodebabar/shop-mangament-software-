570bc62c7d8bb09996e79285d7ed4216f08d5a43
codewithbabar <learncodewithbabar@gmail.com>
2026-02-16 21:21:52 +0500

    fgf

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VITE_LOCAL_IP } from './src/api/Vite_React_Backend_Base'
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
-  const localIP = env.VITE_LOCAL_IP || 'localhost'
+  const localIP = VITE_LOCAL_IP || 'localhost'
 
   return {
     plugins: [react()],
